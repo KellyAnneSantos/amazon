7892e12694d71f5663559cbec1f95014bca63880
Kelly <kelly.a.artola@ricealumni.net>
2022-12-22 20:58:25 -0600

    grouping for heroku

--- backend/routes/api/products.js
+++ backend/routes/api/products.js
@@ -118,6 +118,7 @@ router.get("/:productId", async (req, res) => {
       "updatedAt",
       [sequelize.fn("COUNT", sequelize.col("Reviews.id")), "numReviews"],
     ],
+    group: ["Product.id"],
   });
 
   return res.json(product);
