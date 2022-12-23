edac086dd8c70a1d0ea3b010737acffbf9e0ffd1
Kelly <kelly.a.artola@ricealumni.net>
2022-12-22 21:00:36 -0600

    adding descriptions to grouping for heroku

--- backend/routes/api/products.js
+++ backend/routes/api/products.js
@@ -118,7 +118,7 @@ router.get("/:productId", async (req, res) => {
       "updatedAt",
       [sequelize.fn("COUNT", sequelize.col("Reviews.id")), "numReviews"],
     ],
-    group: ["Product.id"],
+    group: ["Product.id", "Descriptions.id"],
   });
 
   return res.json(product);
