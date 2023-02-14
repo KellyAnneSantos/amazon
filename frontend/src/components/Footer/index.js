6720cdb33e7086c440f83d463520094ca6283478
Kelly <kelly.a.artola@ricealumni.net>
2023-02-14 10:46:38 -0600

    updated links

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -3,11 +3,11 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <div id="footer">
-      <a href="https://github.com/KellyAnneSantos/SoundCloud" target={"_blank"}>
+      <a href="https://github.com/KellyAnneSantos/amazon" target={"_blank"}>
         <img src="/images/githublogo.png" alt="Link to Github" id="git-hub" />
       </a>
       <a
-        href="https://github.com/KellyAnneSantos/SoundCloud"
+        href="https://github.com/KellyAnneSantos/amazon"
         target={"_blank"}
         id="Kelly-Anne-Santos"
       >
